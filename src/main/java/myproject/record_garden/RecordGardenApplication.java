250e97b7e19342b88a3ce7c48c0ada488b401dc2
Uhyunjuk <toearthgold@gmail.com>
2025-02-12 03:16:02 +0900

    :tada: first commit

---
+++ src/main/java/myproject/record_garden/RecordGardenApplication.java
@@ -0,0 +1,13 @@
+package myproject.record_garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordGardenApplication.class, args);
+	}
+
+}
